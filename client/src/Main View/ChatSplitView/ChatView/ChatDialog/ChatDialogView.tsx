5a38114715ac5519486d2ffce21c354b44e52b7c
Ken Guan <17guanr@gmail.com>
2023-09-20 17:08:14 -0700

    style fix

--- client/src/Main View/ChatSplitView/ChatView/ChatDialog/ChatDialogView.tsx
+++ client/src/Main View/ChatSplitView/ChatView/ChatDialog/ChatDialogView.tsx
@@ -111,10 +111,10 @@ class ChatDialogView extends React.Component<ChatDialogProps, ChatDialogState> {
                 {this.props.waitingForResponse && <div className='history-item' style={{ display: 'flex', minHeight: '70px' }} key={-1}>
                     <img src={`/assets/${this.props.model}.png`} width={40} className='avatar' />
                     <div className='message'>
-                        <div style={{width: 20, height: '40px', position: 'relative'}}><Loading size={20}/></div>
+                        <div style={{width: 40, height: '64px', position: 'relative'}}><Loading size={20}/></div>
                     </div>
                 </div>}
-                {(this.props.history.messages.length > 0 || this.props.waitingForResponse) ? dialogCells.reverse() : <ChatSuggestions chatPrompts={this.state.chatPrompts} textToImagePrompts={this.state.textToImagePrompts} onClickPrompt={this.props.onClickPrompt} model={this.props.model}/>}
+                {(!this.shouldDisplayExamplePrompts() || this.props.waitingForResponse) ? dialogCells.reverse() : <ChatSuggestions chatPrompts={this.state.chatPrompts} textToImagePrompts={this.state.textToImagePrompts} onClickPrompt={this.props.onClickPrompt} model={this.props.model}/>}
             </div>
         </div>;
     }
